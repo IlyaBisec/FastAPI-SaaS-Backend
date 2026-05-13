0a2b26ae30ef68c76128028478d0f0b89b1b8281
Ilya-bisec <78878486+IlyaBisec@users.noreply.github.com>
2026-05-13 12:04:59 +0300

    fix: settings module path

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-from config import settings
+from app.core.config import settings
 
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
