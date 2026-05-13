812726a5cc3ff09d95e161cad62617dc38422823
Ilya-bisec <78878486+IlyaBisec@users.noreply.github.com>
2026-05-13 12:05:47 +0300

    add: main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,2 +1,16 @@
 # Main -
 # 13.05.2026 (c) ilya_bisec
+
+from fastapi import FastAPI
+
+from app.api.v1.auth import router as auth_router
+from app.api.v1.tasks import router as tasks_router
+
+app = FastAPI(
+    title="Production FastAPI Backend",
+    version="1.0.0",
+)
+
+
+app.include_router(auth_router)
+app.include_router(tasks_router)
