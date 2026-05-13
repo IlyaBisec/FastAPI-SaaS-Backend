54473b1b96f0ab3698bf6ecf3587fd79e6dcf02f
Ilya-bisec <78878486+IlyaBisec@users.noreply.github.com>
2026-05-13 12:06:51 +0300

    add: user schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,2 +1,20 @@
 # User -
 # 13.05.2026 (c) ilya_bisec
+
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    role: str
+
+    model_config = {
+        "from_attributes": True
+    }
