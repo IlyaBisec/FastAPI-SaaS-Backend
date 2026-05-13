679576d1c30fb86b7bdf7d01bc2919ab0cf973b5
Ilya-bisec <78878486+IlyaBisec@users.noreply.github.com>
2026-05-13 16:35:58 +0300

    add src check

--- backend/fsbsrc/core/security.py
+++ backend/fsbsrc/core/security.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 
-from config import settings
+from fsbsrc.core.config import settings
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
