2b1865a74cae78e799d1293ddac7436603eaeb88
Ilya-bisec <ilya.borisov.bisec@gmail.com>
2026-05-17 21:50:54 +0300

    Update user_service.py

--- backend/fsbsrc/services/user_service.py
+++ backend/fsbsrc/services/user_service.py
@@ -1,2 +1,19 @@
-# User Service -
-# 13.05.2026 (c) ilya_bisec
+# User service.
+# Contains business logic for users.
+# 16.05.2026 (c) ilya_bisec
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fsbsrc.models.user import User
+
+
+class UserService:
+
+    @staticmethod
+    async def get_all_users(db: AsyncSession):
+        result = await db.execute(
+            select(User)
+        )
+
+        return result.scalars().all()
